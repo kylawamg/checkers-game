0d75edabf1be38e8bfb064a268f1afff08cdd2ea
Xamp <deivid.develop@gmail.com>
2020-02-22 17:04:29 +0100

    Added game board

--- js/board.js
+++ js/board.js
@@ -13,15 +13,17 @@ class Board {
     const elementBoard = document.getElementById(this.boardId);
     let color = '';
     for (let i = 0; i < this.rows; i++) {
-      this.boardGrid[i] = [];
+      this.boardGrid[i] = []; 
       for (let j = 0; j < this.columns; j++) {
         if (i % 2 === 0 && j % 2 === 0) {
           color = 'white';
-        } else if (i % 2 !== 0 && j % 2 !== 0) {
+        } else if (i % 2 !== 0 && j % 2 === 0) {
           color = 'brown';
         } else if (i % 2 === 0 && j % 2 !== 0) {
           color = 'brown';
         } else if (i % 2 !== 0 && j % 2 === 0) {
+          color = 'white';
+        } else {
           color = 'white';
         }
         const cell = new Cell(i, j, color);
